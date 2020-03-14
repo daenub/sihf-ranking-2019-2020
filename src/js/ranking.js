106c5b601868e406d696e3bb0b7941fcb2bc3a0a
Dan Büschlen <danbueschlen@gmail.com>
2020-03-14 23:09:33 +0100

    init commit

---
+++ src/js/ranking.js
@@ -0,0 +1,83 @@
+import {teams, gamesByRound} from "../data/games.json"
+const roundList = []
+
+for (let round in gamesByRound) {
+  roundList[parseInt(round)] = gamesByRound[round].values
+}
+
+function getValuesOfTeam(team, rounds) {
+  const teamData = {
+    name: team,
+    points: 0,
+    goalsScored: 0,
+    goalsReceived: 0,
+  }
+
+  return rounds.reduce((acc, round) => {
+    const game = round.find(game => team === game.home || team === game.away)
+
+    if (game) {
+      const isHomeTeam = team === game.home
+      const teamScore = isHomeTeam ? game.homeScore : game.awayScore
+      const opponentScore = isHomeTeam ? game.awayScore : game.homeScore
+
+      teamData.goalsScored += teamScore
+      teamData.goalsReceived += opponentScore
+
+      if (teamScore > opponentScore) {
+        if (game.periods.length === 4) {
+          teamData.points += 2
+        } else {
+          teamData.points += 3
+        }
+      } else {
+        if (game.periods.length === 4) {
+          teamData.points += 1
+        }
+      }
+    }
+
+    return acc
+  }, teamData)
+}
+
+const goalDifference = t => t.goalsScored - t.goalsReceived
+
+function sortRanking(teamA, teamB) {
+  if (teamA.points > teamB.points) {
+    return -1
+  } else if (teamA.points < teamB.points) {
+    return 1
+  } else if (goalDifference(teamA) > goalDifference(teamB)) {
+    return -1
+  } else if (goalDifference(teamA) < goalDifference(teamB)) {
+    return 1
+  } else if (teamA.goalsScored > teamB.goalsScored) {
+    return -1
+  } else if (teamA.goalsScored < teamB.goalsScored) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+const rankingByRound = roundList.map((r, i) => {
+  let ranking = teams.map(t => getValuesOfTeam(t.name, roundList.slice(0, i + 1)))
+  ranking = ranking.sort(sortRanking)
+
+  return ranking
+})
+
+const teamRankingsPerRound = teams.map(t => {
+  return {
+    team: t.name,
+    rankings: rankingByRound.map(round => round.findIndex(ranking => ranking.name === t.name))
+  }
+})
+
+export {
+  teams,
+  roundList,
+  rankingByRound,
+  teamRankingsPerRound
+}
---
+++ src/js/visualization.js
@@ -0,0 +1,188 @@
+import * as d3 from "d3"
+import {
+  teams,
+  roundList,
+  rankingByRound,
+  teamRankingsPerRound,
+} from "./ranking"
+
+
+const teamColors = {
+  "EHCB": "#D11216",
+  "EVZ": "#CFBD72",
+  "GSHC": "#820924",
+  "HCAP": "#022E5F",
+  "HCD": "#FFDD00",
+  "HCFG": "#E9171E",
+  "HCL": "#000000",
+  "LHC": "#E40F29",
+  "SCB": "#E30613",
+  "SCL": "#FFD600",
+  "SCRJ": "#03326D",
+  "ZSC": "#0069B4",
+}
+
+const getAcronym = name => teams.find(t => t.name === name).acronym
+const getLogoIdByName = d => getLogoId({acronym: getAcronym(d.name)})
+const getLogoId = d => "logo-" + d.acronym
+
+const getTeamColor = d => teamColors[getAcronym(d.team)]
+
+const margin = {top: 10, right: 30, bottom: 30, left: 60},
+  width = 1800 - margin.left - margin.right,
+  height = 600 - margin.top - margin.bottom
+
+const svg = d3.select("#ranking-root")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")")
+
+// Logo patterns
+svg.append("defs")
+  .selectAll("pattern")
+  .data(teams)
+  .enter()
+  .append("pattern")
+  .attr("id", getLogoId)
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("width", 40)
+  .attr("height", 40)
+  .append("image")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("width", 40)
+  .attr("height", 40)
+  .attr("xlink:href", d => require(`../images/logos/${d.acronym}.png`))
+  .attr("preserveAspectRatio", "xMinYMin slice")
+
+// Scales
+const x = d3.scaleLinear()
+  .domain([1, roundList.length])
+  .range([ 0, width ])
+
+const y = d3.scaleLinear()
+  .domain([teams.length, 1])
+  .range([ height, 0 ])
+
+// Axis
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
+
+svg.append("g")
+  .call(d3.axisLeft(y))
+
+// Background pattern
+svg.selectAll("rect.rank")
+  .data(teams)
+  .enter()
+  .append("rect")
+  .classed("rank", true)
+  .attr("fill", (d, i) => i % 2 === 0 ? "none" : "#999")
+  .attr("opacity", 0.1)
+  .attr("x", 0)
+  .attr("y", (d, i) => y(i))
+  .attr("width", width)
+  .attr("height", (d, i, nodes) => height / nodes.length)
+
+svg.selectAll("rect.round")
+  .data(roundList)
+  .enter()
+  .append("rect")
+  .classed("round", true)
+  .attr("fill", (d, i) => i % 2 === 0 ? "none" : "#777")
+  .attr("opacity", 0.1)
+  .attr("x", (d, i) => x(i))
+  .attr("y", 0)
+  .attr("width", (d, i) => width / roundList.length)
+  .attr("height", height)
+
+
+// Paths of teams
+const teamPaths = svg.selectAll("path.team")
+  .data(teamRankingsPerRound)
+  .enter()
+  .append("path")
+  .classed("team", true)
+  .attr("fill", "none")
+  .attr("stroke", "#000")
+  .attr("stroke-width", 4)
+  .attr("d", d => {
+      const line = d3.line()
+        .x(function(d, i) { return x(i + 1) })
+        .y(function(d) { return y(d + 1) })
+
+      return line(d.rankings)
+    }
+  )
+  .on("mouseover", teamPathMouseOver)
+  .on("mouseleave", teamPathMouseLeave)
+
+// Ranking numbers
+const teamRanks = svg.selectAll("g.team-ranks")
+  .data(teamRankingsPerRound)
+  .enter()
+  .append("g")
+  .classed("team-ranks", true)
+  .attr("opacity", 0)
+
+teamRanks.each((d, teamIndex, nodes) => {
+  const elemEnter = d3.select(nodes[teamIndex])
+    .selectAll("circle.team-rank")
+    .data(d.rankings)
+    .enter()
+
+  elemEnter.append("circle")
+    .classed("team-rank", true)
+    .attr("fill", "steelblue")
+    .attr("r", 10)
+    .attr("cx", (d, i) => x(i + 1))
+    .attr("cy", (d, i) => y(d + 1))
+    .attr("fill", getTeamColor(d))
+
+  elemEnter.append("text")
+    .classed("team-rank-text", true)
+    .text(d => d + 1)
+    .attr("x", (d, i) => x(i + 1))
+    .attr("y", (d, i) => y(d + 1))
+    .attr("transform", "translate(0, 4)")
+})
+
+// Team logos
+svg.selectAll("circle.team-logo")
+  .data(rankingByRound[0])
+  .enter()
+  .append("circle")
+  .classed("team-logo", true)
+  .attr("cx", -20)
+  .attr("cy", (d, i) => y(i + 1))
+  .attr("r", 20)
+  .attr("fill", (d) => `url(#${getLogoIdByName(d)})`)
+
+function teamPathMouseOver(d, i) {
+  teamPaths.each((d, teamIndex, nodes) => {
+    d3.select(nodes[teamIndex])
+      .attr("opacity", teamIndex === i ? 1 : 0.25)
+  })
+
+  teamRanks.each((d, teamIndex, nodes) => {
+    d3.select(nodes[teamIndex])
+      .attr("opacity", teamIndex === i ? 1 : 0)
+  })
+}
+
+function teamPathMouseLeave() {
+  teamPaths.each((d, teamIndex, nodes) => {
+    d3.select(nodes[teamIndex])
+      .attr("opacity", 1)
+  })
+
+  teamRanks.each((d, teamIndex, nodes) => {
+    d3.select(nodes[teamIndex])
+      .attr("opacity", 0)
+  })
+}
